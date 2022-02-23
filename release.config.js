c6b53f41c3984b1f11d6ab1e663cd88323125f5c
prashant9428 <pathakprashant841@gmail.com>
2022-02-23 15:14:37 +0530

    fix: removed release config

--- release.config.js
+++
@@ -1,9 +0,0 @@
-module.exports  = {
-    branches: ["master",'development','staged'],
-    repositoryUrl:"https://github.com/prashant9428/semantic",
-    plugins:[
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-        '@semantic-release/github'
-    ]
-}
