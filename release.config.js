fff9beeaa2fdb1047037e941c54311ac5a1f264f
prashant9428 <pathakprashant841@gmail.com>
2022-04-11 18:10:24 +0530

    fix: added branch

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports  = {
-    branches: ["master"],
+    branches: ["master","prod"],
     repositoryUrl:"https://github.com/prashant9428/semantic",
     plugins:[
         '@semantic-release/commit-analyzer',
